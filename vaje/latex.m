00ed81f70c0883be08f06a2bb319e9d4c2ec8022
Martin Vuk <martin.vuk@fri.uni-lj.si>
2013-03-29 12:39:30 +0100

    / ‘vaje/latex.m’

--- vaje/latex.m
+++ vaje/latex.m
@@ -2,7 +2,7 @@ function latex(M,file)
 % funkcija latex(M,file)
 % shrani spremenljivko M v datoteko file
 % v obliki matrike primerne za latex
-% M = round(M*100)/100; % zaokrozimo na 2 decimalki
+M = round(M*100)/100; % zaokrozimo na 2 decimalki
 tex = strrep(strrep(mat2str(M)," ","&"),";","\\\\\n")(2:end-1);
 fid = fopen (file, "w");
 fwrite(fid,tex);
