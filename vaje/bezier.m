67076ce62e3630203ff0a045a5966275df094d33
Martin Vuk <martin.vuk@fri.uni-lj.si>
2013-03-30 01:24:20 +0100

    / ‘vaje/bezier.m’

--- vaje/bezier.m
+++ vaje/bezier.m
@@ -1,11 +1,11 @@
 function tocke=bezier(t,P)
 % funkcija tocke=bezier(t,P) 
-% izračune točke na Bezierovi krivulji s 
+% izracuna tocke na Bezierovi krivulji s 
 % kontrolnim poligonom P pri vrednoti parametra t
 % po definiciji z Bernsteinovimi polinomi
 % Vhod
 % t ... tabela parametrov 
-% P ... 2xn tabela kontrolnih točk
+% P ... 2xn tabela kontrolnih tock
 
 % Bernsteinov polinom
 b = @(n,i,t) nchoosek(n,i)*t.^i.*(1-t).^(n-i);
