7f7e26b9a8e42e15b6f602b012dc9cf36975016d
Martin Vuk <martin.vuk@fri.uni-lj.si>
2013-05-14 23:34:07 +0200

    / ‘domace/rk4.m’

--- domace/rk4.m
+++ domace/rk4.m
@@ -1,8 +1,8 @@
-function [y,x]=rk2(f,x0,y0,h,n)
-% funkcija [y,x]=rk2(f,x0,y0,h,n)
+function [y,x]=rk4(f,x0,y0,h,n)
+% funkcija [y,x]=rk4(f,x0,y0,h,n)
 % izračuna približek za začetni problem
 % za NDE y'(x)=f(x,y) z y(x0)=y0
-% z Runge-Kutta metodo reda 2 s korakom h
+% z Runge-Kutta metodo reda 4 s korakom h
 % Vhod
 % f ... desna stran DE
 % x0,y0 ... začetni pogoj y(x0)=y0
@@ -18,6 +18,8 @@ y(:,1)=y0; % zacetni pogoj v y0
 
 for i=1:n
 	k1 = h*f(x(i),y(:,i));
-	k2 = h*f(x(i)+h,y(:,i)+k1);	
-	y(:,i+1)=y(:,i)+(k1+k2)/2;
+	k2 = h*f(x(i)+h/2,y(:,i)+k1/2);
+	k3 = h*f(x(i)+h/2,y(:,i)+k2/2);
+	k4 = h*f(x(i)+h,y(:,i)+k3);	
+	y(:,i+1)=y(:,i) + (k1 + 2*(k2 + k3) + k4)/6;
 end
